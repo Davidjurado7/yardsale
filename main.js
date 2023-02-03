2c154c5bed8f753bff220be2a249de576e20a80b
David Jurado <davidjuradobenito@gmail.com>
2023-02-03 16:52:46 +0100

    Integrated menu desktop and mobile

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
-const nabEmail = document.querySelector('.nab-email')
+const navEmail = document.querySelector('.nab-email')
 const menuDesktop = document.querySelector('.desktop-menu')
+const menuHamMobile = document.querySelector('.menu-icon')
+const menuMobile = document.querySelector('.mobile-menu')
+const carIcon = document.querySelector('.car-container')
+const myOrder = document.querySelector('.my-order-container')
 
-nabEmail.addEventListener('click', toggleMenuDesktop)
+navEmail.addEventListener('click', toggleMenuDesktop)
+menuHamMobile.addEventListener('click', toggleMenuMobile)
+carIcon.addEventListener('click', toggleCarContainer)
 
 function toggleMenuDesktop () {
     menuDesktop.classList.toggle('inactive')
+    myOrder.classList.add('inactive')
+
+}
+
+function toggleMenuMobile () {
+    myOrder.classList.add('inactive')
+    menuMobile.classList.toggle('inactive')
+}
+
+function toggleCarContainer () {
+    menuDesktop.classList.add('inactive')
+    menuMobile.classList.add('inactive')
+    myOrder.classList.toggle('inactive') 
+
 }
