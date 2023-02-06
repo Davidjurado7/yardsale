cf48a78b380674cee6e6b300abcdfa2876801f31
David Jurado <davidjuradobenito@gmail.com>
2023-02-06 17:26:19 +0100

    Integrate complete car with total price

--- main.js
+++ main.js
@@ -11,30 +11,36 @@ const productPrice = document.querySelector('.product-price')
 const productName = document.querySelector('.product-name')
 const productImage = document.querySelector('.product-image')
 const productDescription = document.querySelector('.product-description')
+const orderAddContainer = document.querySelector('.order-add-container')
+const totalPrice = document.querySelector('.total-price p')
+const numberofproduct = document.querySelector('.number-of-product')
 
 const productList = []
 
+let totalOrderPrice = 0
+let productListOrder = []
+
 productList.push({
     name: 'Bike',
-    price: "120 €",
+    price: 120,
     img: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     description: 'bicycle, also called bike, two-wheeled steerable machine that is pedaled by the riders feet. On a standard bicycle the wheels are mounted in-line in a metal frame, with the front wheel held in a rotatable fork.',
 })
 productList.push({
     name: 'Ipad',
-    price: "500 €",
+    price: 500,
     img: 'https://images.pexels.com/photos/1334597/pexels-photo-1334597.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     description: 'iPad mini is meticulously designed to be absolutely beautiful. An all-new enclosure features a new, larger edge-to-edge screen, along with narrow borders and elegant rounded corners.'
 })
 productList.push({
     name: 'Mouse',
-    price: "50 €",
+    price: 50,
     img: 'https://images.pexels.com/photos/2115256/pexels-photo-2115256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     description: 'The mouse is a small, movable device that lets you control a range of things on a computer. Most types of mouse have two buttons, and some will have a wheel in between the buttons.'
 })
 productList.push({
     name: 'Laptop',
-    price: "700 €",
+    price: 700,
     img: 'https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
     description: 'A laptop computer, sometimes called a notebook computer by manufacturers, is a battery- or AC-powered personal computer generally smaller than a briefcase that can easily be transported and conveniently used in temporary spaces such as on airplanes, in libraries, temporary offices, and at meetings.'
 })
@@ -62,8 +68,8 @@ function toggleCarContainer() {
     menuDesktop.classList.add('inactive')
     menuMobile.classList.add('inactive')
     productDetails.classList.add('inactive')
-    myOrderContainer.classList.toggle('inactive') 
-    
+    myOrderContainer.classList.toggle('inactive')
+
 }
 
 function openProductDetails(imgSrc) {
@@ -84,8 +90,46 @@ function productClosed() {
     productDetails.classList.add('inactive')
 }
 
-function renderProductCar(product) {
-    console.log(product)
+function renderProductCar(productSelect) {    
+    const productAdd = productList.filter(product => product.name == productSelect.id)
+    productListOrder.push(productAdd)
+
+    numberofproduct.innerText = productListOrder.length
+
+    const articlesContainer = document.createElement('div')
+    articlesContainer.classList.add('articles-container')
+    orderAddContainer.appendChild(articlesContainer)
+
+    const article = document.createElement('div')
+    article.classList.add('article')
+    articlesContainer.appendChild(article)
+
+    const articleDiv = document.createElement('div')
+    article.appendChild(articleDiv)
+
+    const articleImg = document.createElement('img')
+    articleImg.setAttribute('src', productAdd[0].img)
+    articleImg.classList.add('article-img')
+    articleDiv.appendChild(articleImg)
+
+    const articleP = document.createElement('p')
+    articleP.innerText = (productAdd[0].name)
+    articleDiv.appendChild(articleP)
+
+    const section = document.createElement('section')
+    article.appendChild(section)
+
+    const sectionP = document.createElement('p')
+    sectionP.innerText = `${(productAdd[0].price)} €`
+    totalOrderPrice = totalOrderPrice + productAdd[0].price
+    totalPrice.innerText = `${totalOrderPrice} €`
+    section.appendChild(sectionP)
+    
+    const sectionIcon = document.createElement('img')
+    sectionIcon.setAttribute('src', './Icons/icon_close.png')
+    sectionIcon.classList.add('close')
+    section.appendChild(sectionIcon)
+
 }
 
 function renderProducts(arr) {
@@ -123,7 +167,12 @@ function renderProducts(arr) {
         
         const productFigureImg = document.createElement('img')
         productFigureImg.setAttribute('src', './Icons/bt_add_to_cart.svg')
+        productFigure.setAttribute('id', product.name)
         productFigure.appendChild(productFigureImg)
+        const productSelect = document.getElementById(product.name)
+        productFigure.addEventListener('click', product => {
+            renderProductCar(productSelect)
+        })    
         
     }
 }
